1e627b873ea68b2e3328f70bd331146a5f9ba242
Lenovo <Lenovo@DESKTOP-2IQFPJK>
2026-05-01 20:18:55 +0200

    Add project files.

---
+++ CodingWiki_Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using CodingWiki_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
