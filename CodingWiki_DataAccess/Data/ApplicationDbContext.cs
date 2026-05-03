bf4557e1e537481dcc13d36ec22554b7fc1ad1fb
uendi-123 <uendivocaj4@gmail.com>
2026-05-03 17:50:48 +0200

    add book model

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingWiki_Model.Models
+{
+    public class Book
+    {
+        public int IDBook { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }    
+        public double Price { get; set; }
+    }
+}
