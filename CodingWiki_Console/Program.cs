df4e6688293884d9376f3eb5b025142f077c94a4
uendi-123 <uendivocaj4@gmail.com>
2026-05-06 21:48:57 +0200

    add async methods

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -14,16 +14,47 @@ Console.WriteLine("Hello, World!");
 //}
 //AddBook();
 //GetAllBook();
-GetBook();
-void GetBook()
+//GetBook();
+//UpdateBook();
+DeleteBook();
+
+async void DeleteBook()
+{
+    using var context = new ApplicationDbContext();
+    var book = await context.Books.FindAsync(6);
+    context.Books.Remove(book);
+    await context.SaveChangesAsync();
+    
+}
+
+
+async void UpdateBook()
+{
+    try
+    {
+        using var context = new ApplicationDbContext();
+        var books = await context.Books.Where(u=>u.Publisher_ID==1).ToListAsync();
+        foreach(var book in books)
+        {
+            book.Price += 5;
+        }
+        await context.SaveChangesAsync();
+    } catch (Exception e)
+    {
+    }
+}
+
+async void GetBook()
 {
     try
     {
         using var context = new ApplicationDbContext();
         //var book = context.Books.Find(6); //usato per l'id
         //var book=context.Books.Single(u=>u.Publisher_ID==3); //Deve restiture un solo risultato, altrimenti lancia un'eccezione
-        var books = context.Books.Where(u=>u.ISBN.Contains("12"));
+        //var books = context.Books.Where(u=>u.ISBN.Contains("12"));
         //Console.WriteLine(book.Title + " - " + book.ISBN);
+
+        var books = await context.Books.OrderBy(u => u.Title).ToListAsync();
         foreach (var book in books)
         {
             Console.WriteLine(book.Title + " - " + book.ISBN);
@@ -44,10 +75,10 @@ void GetAllBook()
 }
 
 
-void AddBook()
+async void AddBook()
 {
     Book book = new Book { Title = "New EF Core Book",ISBN="121212223",Price=10.93m,Publisher_ID=1};
     using var context = new ApplicationDbContext();
-    var books = context.Books.Add(book);
-    context.SaveChanges();
+    var books =await context.Books.AddAsync(book);
+    context.SaveChangesAsync();
 }
