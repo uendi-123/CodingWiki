4235b1c84278b892f17780267c00c553d8276fe0
uendi-123 <uendivocaj4@gmail.com>
2026-05-06 19:24:44 +0200

    get book

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+//using (ApplicationDbContext context = new())
+//{
+//context.Database.EnsureCreated();
+//if(context.Database.GetPendingMigrations().Count() > 0) 
+//{
+//context.Database.Migrate();
+//}
+//}
+
+GetAllBook();
+void GetAllBook()
+{
+    using var context = new ApplicationDbContext();
+    var books = context.Books.ToList();
+    foreach (var book in books) 
+    {
+        Console.WriteLine(book.Title+" - "+book.ISBN);
+    }
+}
