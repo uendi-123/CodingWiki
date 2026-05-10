85fdcd0439096cdf5db3756106c8daed0b1e3965
uendi-123 <uendivocaj4@gmail.com>
2026-05-10 13:27:17 +0200

    add BookView

---
+++ CodingWiki_Model/ViewModels/BookVM.cs
@@ -0,0 +1,14 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingWiki_Model.ViewModels
+{
+    public class BookVM
+    {
+        public Book Book { get; set; }
+        public IEnumerable<SelectListItem> PublisherList { get; set; }
+    }
+}
---
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using CodingWiki_Model.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class BookController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Book> objList = _db.Books.ToList();
+            return View(objList);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            BookVM obj = new();
+            obj.PublisherList = _db.Publishers.Select(i => new SelectListItem
+            {
+                Text=i.Name,
+                Value=i.Publisher_Id.ToString()
+
+            });
+            
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //edit
+            obj.Book= _db.Books.FirstOrDefault(u => u.BookID == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
+}
